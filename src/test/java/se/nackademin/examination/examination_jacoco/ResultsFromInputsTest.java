46474cc42ff0d2aacab88a91bd51f9f2cf12b9cd
Mattias Svensson <matte_sesso@hotmail.com>
2016-10-14 14:59:26 +0200

    Add tests to make 100% codecoverage on class ResultsFromInputs

--- src/test/java/se/nackademin/examination/examination_jacoco/ResultsFromInputsTest.java
+++ src/test/java/se/nackademin/examination/examination_jacoco/ResultsFromInputsTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 public class ResultsFromInputsTest {
 
 	@Test
-	public void testAgeGettersAndSettersMethods() {
+	public void testNameLenghtGettersAndSettersMethods() {
 		ResultFromInputs resultsFromInputs = new ResultFromInputs();
 		resultsFromInputs.setResultForNameLenght(10);
 		assertEquals("The result should be 10", resultsFromInputs.getResultForNameLenght(), 10);
@@ -19,5 +19,17 @@ public class ResultsFromInputsTest {
 		resultsFromInputs.setResultForGender(10);;
 		assertEquals("The result should be 10", resultsFromInputs.getResultForGender(), 10);
 	}
-
+	
+	@Test
+	public void testAgeGettersAndSettersMethods() {
+		ResultFromInputs resultsFromInputs = new ResultFromInputs();
+		resultsFromInputs.setResultForAge(10);
+		assertEquals("The result should be 10", resultsFromInputs.getResultForAge(), 10);
+	}
+	@Test
+	public void testHomeGettersAndSettersMethods() {
+		ResultFromInputs resultsFromInputs = new ResultFromInputs();
+		resultsFromInputs.setResultForHomeCity(10);
+		assertEquals("The result should be 10", resultsFromInputs.getResultForHomeCity(), 10);
+	}
 }

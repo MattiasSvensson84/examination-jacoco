f57b2f9da07fb1431d5f56df76ebc291bc8a9215
Mattias Svensson <matte_sesso@hotmail.com>
2016-10-14 14:15:34 +0200

    Add tests so that is  100% codecoverage on class Conversor

--- src/test/java/se/nackademin/examination/examination_jacoco/ConversorTest.java
+++ src/test/java/se/nackademin/examination/examination_jacoco/ConversorTest.java
@@ -5,12 +5,31 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class ConversorTest {
-
+	
+	Conversor conversor = new Conversor();
 	@Test
-	public void testConversor() {
-		Conversor conversor = new Conversor();
+	public void testConversorGetSetName() {
+		//Conversor conversor = new Conversor();
 		conversor.setNameConverterArrayValues();
 		assertEquals(conversor.getNameConverterArray().get(0), "Your first name is smaller than your last name");
+		
 	}
-
+	
+	@Test
+	public void testConversorGetSetGender(){
+		conversor.setGenderConverterArrayValues();
+		assertEquals(conversor.getGenderConverterArray().get(0), "man!");
+	}
+	
+	@Test
+	public void testConversorGetSetAge(){
+		conversor.setAgeConverterArrayValues();
+		assertEquals(conversor.getAgeConverterArray().get(0), "You are young and talented! Things will come your way soon enough! ");
+	}
+	
+	@Test
+	public void testConversorGetSetCity(){
+		conversor.setCityConverterArrayValues();
+		assertEquals(conversor.getCityConverterArray().get(0), " A, ");
+	}
 }
--- src/test/java/se/nackademin/examination/examination_jacoco/DataAnalysisTest.java
+++ src/test/java/se/nackademin/examination/examination_jacoco/DataAnalysisTest.java
@@ -9,15 +9,26 @@ import org.junit.Test;
 
 public class DataAnalysisTest {
 
+	DataAnalysis dataAnalysis = new DataAnalysis();
 	@Test
-	public void test1() {
-		DataAnalysis dataAnalysis = new DataAnalysis();
+	public void testBuildFinalString() {
+		//DataAnalysis dataAnalysis = new DataAnalysis();
 		ArrayList<String> values = new ArrayList<String>();
 		values.addAll(Arrays.asList("Game", "Rafael", "Silva", "M", "30", "Fortaleza"));
 		String result = dataAnalysis.buildFinalString(values);
 		assertTrue("The result should contain 'The name of the homecity is big and the participant is 30 or older' ",
 				result.contains("The name of the homecity is big and the participant is 30 or older"));
 
+	}
+	
+	@Test
+	public void testGetResultStringNamesAndAgeUnderEquals30(){
+		
+		ArrayList<String> values = new ArrayList<String>();
+		values.addAll(Arrays.asList("Game", "Rafael", "Silva", "M", "30", "Fortaleza"));
+		String result = dataAnalysis.getResultStringNamesAndAge(values);
+		assertTrue("The first name is smaller or equals in size to the last name and the participant is 30 or younger",
+				result.contains("participant is 30 or younger"));
 	}
 
 }
